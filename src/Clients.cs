98111025a604ebb380ce4e6933f83c562c02fd77
Bora Aydın <mehmetbora.aydin@dk.akbank.com>
2020-09-01 20:12:53 +0300

    calisir halde

--- src/Clients.cs
+++ src/Clients.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using IdentityServer4;
 using IdentityServer4.Models;
 
 internal class Clients
@@ -14,6 +15,26 @@ internal class Clients
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, // change me!
                 AllowedScopes = new List<string> {"api1.read"}
+            },
+            new Client
+            {
+                ClientId = "oidcClient",
+                ClientName = "Example Client Application",
+                ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, // change me!
+                
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = new List<string> {"https://localhost:5002/signin-oidc"},
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "role",
+                    "api1.read"
+                },
+
+                RequirePkce = true,
+                AllowPlainTextPkce = false
             }
         };
     }
--- src/Startup.cs
+++ src/Startup.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using IdentityServer4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -24,26 +18,22 @@ namespace identityserver
                 .AddInMemoryApiScopes(Resources.GetApiScopes())
                 .AddTestUsers(Users.Get())                     
                 .AddDeveloperSigningCredential();
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
             app.UseIdentityServer();
-            app.UseEndpoints(endpoints =>
-            {
-                //endpoints.MapDefaultControllerRoute();
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
         }
     }
 }
